4d5737f8465dac7a8784c54f1c3dc262254e9fe3
tcortes55 <35512873+tcortes55@users.noreply.github.com>
2019-06-30 16:35:01 +0200

    Moving file IO to functions

--- JogoDaVelha/Controllers/GameController.cs
+++ JogoDaVelha/Controllers/GameController.cs
@@ -46,6 +46,7 @@ namespace JogoDaVelha.Controllers
             myGame.map[1] = emptyRow;
             myGame.map[2] = emptyRow;
 
+            // Determinamos aleatoriamente se X ou O irá começar o jogo
             Random random = new Random();
             int randomNumber = random.Next();
             if (randomNumber % 2 == 0)
@@ -57,8 +58,10 @@ namespace JogoDaVelha.Controllers
                 myGame.firstPlayer = "O";
             }
 
+            // A primeira jogada será feita pelo jogador sorteado
             myGame.nextPlayer = myGame.firstPlayer;
 
+            // O jogo é salvo num arquivo XML. O nome do arquivo corresponde ao ID do jogo
             XmlSerializer xsMyGame = new XmlSerializer(typeof(Game));
             string path = @"c:\temp\dti\" + myGame.id + ".xml";
             TextWriter tw = new StreamWriter(path);
@@ -72,48 +75,43 @@ namespace JogoDaVelha.Controllers
         [Route("game/{id}/movement")]
         public IHttpActionResult Post([FromBody]dynamic value)
         {
+            // Obtemos os valores da request...
             string id = value.id;
             string player = value.player.ToString().ToUpper();
             int positionX = value.position.x;
             int positionY = value.position.y;
 
+            // Somente os valores "X" e "O" são aceitos para a jogada
             if (player != "X" && player != "O")
             {
                 return Ok(new { msg = "Jogada inválida; jogue um X ou O" });
             }
 
+            // Se o arquivo <ID>.xml não for encontrado, a partida não existe
             string path = @"c:\temp\dti\" + id + ".xml";
             if (!File.Exists(path))
             {
                 return Ok(new { msg = "Partida não encontrada" });
             }
+            
+            Game myGame = GetGameFromFile(path);
 
-            var sr = new StreamReader(path);
-            XmlSerializer xsMyGame = new XmlSerializer(typeof(Game));
-            Game myGame = (Game)xsMyGame.Deserialize(sr);
-            sr.Close();
-
-            var message = CheckBeforeMovement(myGame);
-
+            // Antes de executar a jogada, fazemos algumas verificações
             if (IsFinished(myGame))
             {
-                return Ok(new { status = "Partida finalizada", winner = myGame.winner });
+                return Ok(new { status = "Partida finalizada", myGame.winner });
             }
-
             if (myGame.nextPlayer != player)
             {
                 return Ok(new { msg = "Não é turno do jogador" });
             }
-
             if (myGame.map[positionX][positionY] != null)
             {
                 return Ok(new { msg = string.Format("Posição [{0},{1}] já foi marcada; jogue em outra posição!", positionX, positionY) });
             }
 
-
-
+            // Jogada será executada...
             myGame.map[positionX][positionY] = player;
-
             switch (player)
             {
                 case "X":
@@ -126,25 +124,37 @@ namespace JogoDaVelha.Controllers
 
             myGame.numRounds++;
 
+            // Após cada jogada, verificamos se há um vencedor
             myGame.winner = CheckForWinner(myGame);
 
-            TextWriter tw = new StreamWriter(path);
-            xsMyGame.Serialize(tw, myGame);
-            tw.Close();
+            SaveGameToFile(myGame, path);
 
             if (IsFinished(myGame))
             {
-                return Ok(new { status = "Partida finalizada", winner = myGame.winner });
+                return Ok(new { status = "Partida finalizada", myGame.winner });
             }
-
-            return Ok(new { code = 200 });
+            else
+            {
+                return Ok(new { code = 200 });
+            }
         }
 
-        private string CheckBeforeMovement(Game myGame)
+        private Game GetGameFromFile(string path)
         {
-            var message = string.Empty;
+            var sr = new StreamReader(path);
+            XmlSerializer xsMyGame = new XmlSerializer(typeof(Game));
+            Game myGame = (Game)xsMyGame.Deserialize(sr);
+            sr.Close();
 
-            return message;
+            return myGame;
+        }
+
+        void SaveGameToFile(Game myGame, string path)
+        {
+            TextWriter tw = new StreamWriter(path);
+            XmlSerializer xsMyGame = new XmlSerializer(typeof(Game));
+            xsMyGame.Serialize(tw, myGame);
+            tw.Close();
         }
 
         private bool IsFinished(Game game)
@@ -210,20 +220,5 @@ namespace JogoDaVelha.Controllers
 
             return winner;
         }
-
-        //// POST: api/Game
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT: api/Game/5
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE: api/Game/5
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
